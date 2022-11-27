917dcfe9fb4b0778682290d4d776987a45cab88a
Ildar Mulyukov <ildar@altlinux.ru>
2022-11-28 02:26:19 +0600

    add closure operator

--- fca/init.lua
+++ fca/init.lua
@@ -1,3 +1,5 @@
+require "fun" ()
+
 local fca ={}
 fca.context = {}
 
@@ -40,6 +42,42 @@ function fca.context.from_csv(fname, delim)
   end
   
   return ctxt
+end
+
+local function contains(A, B) -- A contains B, i.e. B is a subset of A ?
+  return foldl(
+    function(acc, a, b)
+      if b then return acc and a else return acc end
+    end,
+    true, zip(A,B) )
+end
+
+function fca.closure(ctxt, x)
+  -- get the set of common attr for all the objs in x
+  local attrs = totable(
+    foldl(
+      function(acc, obj_no)
+        return map(
+          function (a,b)
+            return a and b
+          end,
+          zip(acc,ctxt[obj_no]) )
+      end,
+      ctxt[x[1]], x) )
+    
+  -- filter all objs that has the attrs
+  local ftr = filter(
+    function(i, obj)
+      return contains(obj, attrs)
+    end,    
+    enumerate(ctxt) )
+  --return only indices
+  return totable(
+    map(
+      function(i)
+        return i
+      end,
+      ftr) )
 end
 
 return fca
---
+++ spec/calc_spec.lua
@@ -0,0 +1,24 @@
+local fca = require 'fca'
+
+describe("FCA module calculating functions", function()
+  local context
+  setup(function()
+      context = fca.context.from_csv("spec/animals.csv", ";")
+    end)
+  
+  it("can do closures",
+    function()
+      local x = { 1 }
+      local c_x = fca.closure(context, x)
+      assert.is_table(c_x)
+      assert.is.equal(1, #c_x)
+      assert.is.equal(c_x[1], x[1])
+      x = { 6 }
+      c_x = fca.closure(context, x)
+      assert.is_table(c_x)
+      assert.is.equal(4, #c_x)
+      assert.is_not.equal(c_x[1], x[1])
+    end)
+
+end)
+  

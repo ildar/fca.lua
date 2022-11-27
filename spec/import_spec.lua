dae328256b960265c2b3873facf8c71df1bb68eb
Ildar Mulyukov <ildar@altlinux.ru>
2022-11-27 14:30:56 +0600

    replace the test CSV file

--- spec/import_spec.lua
+++ spec/import_spec.lua
@@ -1,13 +1,13 @@
 local fca = require 'fca'
 
 describe("FCA module importing function", function()
-  it("can import humen.csv",
+  it("can import animals.csv",
     function()
-      local context = fca.context.from_csv("spec/humen.csv", ";")
+      local context = fca.context.from_csv("spec/animals.csv", ";")
       assert.is_table(context.attrs)
       assert.is.equal(#context.attrs, 4)
       assert.is_table(context.objs)
-      assert.is.equal(#context.objs, 4)
+      assert.is.equal(#context.objs, 7)
     end)
 
 end)

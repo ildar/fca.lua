887b087cefa999ab7daf5c7f60a3ef002c797099
Ildar Mulyukov <ildar@altlinux.ru>
2022-11-30 12:31:40 +0600

    add sets.contains()
    (by table value)

--- fca/sets.lua
+++ fca/sets.lua
@@ -10,4 +10,20 @@ function sets.contains_by_index(A, B) -- A contains B, i.e. B is a subset of A ?
     true, zip(A,B) )
 end
 
+function sets.contains(A, B) -- A contains B, i.e. B is a subset of A ?
+  return foldl(
+    function(acc, b)
+      return acc and foldl(
+        function(acc, a)
+          if a==b then
+            return true
+          else
+            return acc
+          end
+        end,
+        false, A)
+    end,
+    true, B )
+end
+
 return sets
--- spec/sets_spec.lua
+++ spec/sets_spec.lua
@@ -10,6 +10,12 @@ describe("FCA module math sets function", function()
       A = { true,false,false }
       B = { false,true,false }
       assert.falsy(sets.contains_by_index(A,B))
+      A = { 1,2,3 }
+      B = { 2,3 }
+      assert.truthy(sets.contains(A,B))
+      A = { 1,2,3 }
+      B = { 2,3,4 }
+      assert.falsy(sets.contains(A,B))
     end)
 
 end)
